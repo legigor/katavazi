0bb2adb2b08b5be84eec83604657b1a49d4c34d2
Igor Tamashchuk <legigor@gmail.com>
2015-10-11 22:57:24 +0300

    Add a F# playground project

---
+++ FSharpSandbox/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpSandbox
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FSharpSandbox")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Igor Tamashchuk")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
